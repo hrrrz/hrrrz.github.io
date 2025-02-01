ecf82b306c3410f421b88c2f87133cd98ffcad7e
hrrrz <haoranz@ocadu.ca>
2025-01-31 23:54:29 -0500

    Lab 1

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Hello");
+console.log("Current time:", now.toLocaleTimeString());
+
+console.log("Window Width:", window.innerWidth);
+console.log("Window Height:", window.innerHeight);
