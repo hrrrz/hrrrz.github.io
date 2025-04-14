f692ec892c6d858443b049504780a049484c3036
hrrrz <haoranz@ocadu.ca>
2025-04-13 21:19:30 -0400

    delete lab 1

--- lab1/script.js
+++
@@ -1,5 +0,0 @@
-console.log("Hello");
-console.log("Current time:", now.toLocaleTimeString());
-
-console.log("Window Width:", window.innerWidth);
-console.log("Window Height:", window.innerHeight);
